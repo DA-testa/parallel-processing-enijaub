014fd5f4563d9904cb1fcd05fbcf811d30c9710a
221RDB263 <125553206+enijaub@users.noreply.github.com>
2023-03-18 00:10:49 +0200

    Update main.py
    221RDB263 Enija Undīne Brakše 5.grupa

--- main.py
+++ main.py
@@ -2,29 +2,49 @@
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
+    # write the function for simulating parallel tasks, 
     # create the output pairs
 
+    # izveido virkņu sarakstu, lai sekotu pavedienu apstrādes laikam un pavedienu indeksam
+    threads = [(0, i) for i in range(n)]
+
+    #iziet cauri katram darbam
+    for i in range(m):
+        # saņem darba apstrādes laiku
+        job_time = data[i]
+
+        # atrod pavedienu ar vismazāko apstrādes laiku
+        next_thread = min(threads)
+
+        # pievieno izvadei pavediena indeksu un sākuma laiku
+        output.append((next_thread[1], next_thread[0]))
+
+        # atjaunina apstrādes laiku izvēlētajam pavedienam
+        threads.remove(next_thread)
+        threads.append((next_thread[0] + job_time, next_thread[1]))
+        
     return output
 
 def main():
-    # TODO: create input from keyboard
+    # create input from keyboard
     # input consists of two lines
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+
+    n, m = map(int, input().split())
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
 
-    # TODO: create the function
+    data = list(map(int, input().split()))
+
+    # create the function
     result = parallel_processing(n,m,data)
     
-    # TODO: print out the results, each pair in it's own line
-
+    # print out the results, each pair in it's own line
+    for i in range(m):
+        print(result[i][0], result[i][1])
 
 
 if __name__ == "__main__":
